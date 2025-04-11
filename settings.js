b9ec6a1b952725e4e15daa96fe7d8f72d5fdc854
depressednab <depressednab@gmail.com>
2025-04-11 06:05:08 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'el mundo supreme',
+  author: '𓁩NͪAͣBͨᵏꦃͤꦿͬ💔༒⃟۝⃟⁩〠🧍🏾‍♂️🜲⁩',
+  botName: "𓁩NͪAͣBͨᵏꦃͤꦿͬ🐦Bot༒⃟۝⃟⁩〠🧍🏾‍♂️🜲",
+  botOwner: '𓁩NͪAͣBͨᵏꦃͤꦿͬ💔༒⃟۝⃟⁩〠🧍🏾‍♂️🜲', // Your name
+  ownerNumber: '27799191911', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
